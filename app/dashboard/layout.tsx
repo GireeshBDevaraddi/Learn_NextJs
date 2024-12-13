d384c3a1e1098c6c537d56138dc6241adfdd9f2f
GitHubAction <gireesh.devaraddi@maersk.com>
2024-12-13 17:42:09 +0530

    Done

--- app/dashboard/layout.tsx
+++ app/dashboard/layout.tsx
@@ -1,6 +1,5 @@
 import React from "react";
 import SideNav from "@/app/ui/dashboard/sidenav";
-
 const Layout: React.FC<{ children: React.ReactNode }> = ({children}) => {
     return (
         <div className="flex h-screen flex-col md:flex-row md:overflow-hidden">
